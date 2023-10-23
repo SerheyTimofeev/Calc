3762bfe42627918cdf5fffd7fa58c5d3128353e1
Serhey Timofeev <timofeevmore@gmail.com>
2023-10-23 19:54:13 +0300

    less5 update

--- less 5.py
+++ less 5.py
@@ -1,44 +1,51 @@
-m = "0123456789"
-print(m[2])
-#######
-m = "0123456789"
-print(m[-2])
-#######
-m = "0123456789"
-print(m[:5])
-#######
-m = "0123456789"
-print(m[:-2])
-#######
-m = "0123456789"
-print(m[::2])
-#######
-m = "0123456789"
-print(m[1::2])
-#######
-m = "0123456789"
-print(m[::-1])
-#######
-m = "0123456789"
-print(m[::-2])
-#######
-m = "0123456789"
-print(len(m))
-#######
-m = "hello world fdgjd hvhgvkc jhbgkhvkhgv"
-print(m.count(" ") + 1)
-########
-m = "I dont understand"
-print(len(m.split()))
-#######
-s = input()
-ch = input()
-index = -1
-while True:
-    index = s.find(ch, index + 1)
-    if index == -1:
-        break
-    print(index)
-#########
-s = "hhfjsfhjhhhh"
-print(s[0] + s[1:-2].replace("h", "H") + s[-1])
+# m = "0123456789"
+# print(m[2])
+# #######
+# m = "0123456789"
+# print(m[-2])
+# #######
+# m = "0123456789"
+# print(m[:5])
+# #######
+# m = "0123456789"
+# print(m[:-2])
+# #######
+# m = "0123456789"
+# print(m[::2])
+# #######
+# m = "0123456789"
+# print(m[1::2])
+# #######
+# m = "0123456789"
+# print(m[::-1])
+# #######
+# m = "0123456789"
+# print(m[::-2])
+# #######
+# m = "0123456789"
+# print(len(m))
+# #######
+# m = "hello world fdgjd hvhgvkc jhbgkhvkhgv"
+# print(m.count(" ") + 1)
+# ########
+# m = "I dont understand"
+# print(len(m.split()))
+# #######
+# s = input()
+# ch = input()
+# index = -1
+# while True:
+#     index = s.find(ch, index + 1)
+#     if index == -1:
+#         break
+#     print(index)
+# #########
+s = "1234hh567"
+start_index = s.find("h")
+end_index = s.rfind("h")
+print(start_index)
+print(end_index)
+if start_index == -1 or start_index == end_index:
+    print(s)
+else:
+    print(s[:start_index + 1] + s[start_index + 1:end_index].replace("h", "H") + s[end_index:])
