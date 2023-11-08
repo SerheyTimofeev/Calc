be287c56a5d4213ef22687bf46ee776cf14e5948
Serhey Timofeev <timofeevmore@gmail.com>
2023-11-08 21:39:24 +0200

    less_10.new

--- less_10.py
+++ less_10.py
@@ -1,3 +1,4 @@
+import datetime
 # def domains(domen: str) -> list:
 #     """Read domains from file and return list of domains"""
 #     my_file = open(domen, 'r')
@@ -36,10 +37,36 @@
 #             my_split = i.split("-")
 #             if len(my_split) < 2:
 #                 continue
-#             result.append({"data": my_split[0]})
+#             result.append({"date": my_split[0]})
 #         return result
 # print(get_dates_authors("authors.txt"))
 
 #############
 
+def get_dates_authors(file_name: str) -> list:
+    with open(file_name, 'r') as my_file:
+        data = my_file.readlines()
+        result =[]
+        list_for_replace = ["st", "nd", "rd", "th"]
+        for i in data:
+            my_split = i.split("-")
+            if len(my_split) < 2:
+                continue
+            splited_date = my_split[0].strip()
+            day = splited_date.split(" ")[0]
+            result_str = splited_date.split(" ")[1:]
 
+            for n in list_for_replace:
+                day = day.replace(n, "")
+
+            result_str.insert(0, day)
+
+            if len(result_str) < 3:
+                continue
+            result_str = " ".join(result_str)
+
+            formated_date = datetime.datetime.strptime(result_str.strip(), "%d %B %Y").date().strftime("%d/%m/%Y")
+            result.append({"date_original": splited_date, "date_modified": formated_date})
+        return result
+
+print(get_dates_authors("authors.txt"))
